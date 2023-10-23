09d6c1d2cab28bc6aa65fbb93c13c5e9622a5363
Viktor Sec <hey@viktorsec.com>
2023-10-23 04:33:36 +0200

    add Screen component and mask support

--- adventure/src/App.jsx
+++ adventure/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import room1 from './assets/room-1.jpg';
+import room1mask from './assets/room-1-mask.png';
+import { Screen } from './Screen';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Screen
+        image={room1}
+        mask={room1mask}
+        onHovers={[
+          () => console.log("Window"),
+          () => console.log("Door"),
+          () => console.log("Cupboard"),
+          () => console.log("Oil lamp"),
+        ]}
+        onClicks={[
+          () => console.log("The window won't budge"),
+          () => console.log("The door won't budge"),
+          () => console.log("You found a key!"),
+          () => console.log("You took the oil lamp!"),
+        ]}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ adventure/src/Screen/Screen.jsx
@@ -0,0 +1,105 @@
+import React, { useRef } from 'react';
+
+const Screen = ({
+  image,
+  mask,
+  onHovers,
+  onClicks,
+}) => {
+
+  const imageRef = useRef();
+
+  const getColorAtPos = (x, y) => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const image = document.getElementById("mask");
+
+    // Set the canvas dimensions to match the image
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    // Draw the image onto the canvas
+    context.drawImage(image, 0, 0, image.width, image.height);
+
+    // Get the image data for the (X, Y) pixel
+    const imageData = context.getImageData(x, y, 1, 1).data;
+
+    // Extract RGB values
+    const red = imageData[0];
+    //const green = imageData[1];
+    //const blue = imageData[2];
+
+    return red
+  };
+
+  const callCallback = (value, callbacks) => {
+    // not sure what to say about this other than apologize
+    switch (value) {
+      case 250:
+        callbacks[0] && callbacks[0]();
+        break;
+      case 200:
+        callbacks[1] && callbacks[1]();
+        break;
+      case 150:
+        callbacks[2] && callbacks[2]();
+        break;
+      case 100:
+        callbacks[3] && callbacks[3]();
+        break;
+      case 50:
+        callbacks[4] && callbacks[4]();
+        break;
+      case 0:
+        callbacks[5] && callbacks[5]();
+        break;
+    }
+  }
+
+  const getMaskColorBasedOnEvent = (event) => {
+    const { left, top } = imageRef.current.getBoundingClientRect();
+    const offsetX = Math.floor(left);
+    const offsetY = Math.floor(top);
+    
+    const x = event.clientX - offsetX;
+    const y = event.clientY - offsetY;
+    
+    return getColorAtPos(x, y);
+  };
+
+  const handleMouseMove = (event) => {
+    const color = getMaskColorBasedOnEvent(event);
+    callCallback(color, onHovers);
+  };
+
+  const handleClick = (event) => {
+    const color = getMaskColorBasedOnEvent(event);
+    callCallback(color, onClicks);
+  }
+
+  return (
+    <>
+      <img
+        ref={imageRef}
+        src={image}
+        width={1080}
+        height={720}
+        alt="game"
+        onMouseMove={handleMouseMove}
+        onClick={handleClick}
+      />
+      <img
+        id="mask"
+        src={mask}
+        alt="internal image"
+        style={{display: "none"}}
+      /> 
+      <canvas
+        id="canvas"
+        style={{display: "none"}}
+      />
+    </>
+  );
+};
+
+export { Screen };

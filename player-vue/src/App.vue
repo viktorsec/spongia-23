543c96943967eaab5a6bc02a0b6da8cadff0997a
mravcak <alex.mravcak@gmail.com>
2023-10-30 15:58:19 +0100

    player-vue wip

--- player-vue/src/App.vue
+++ player-vue/src/App.vue
@@ -1,47 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="interface">
+    <div class="interface-main">
+      <div class="viewport">
+        <img src="@/assets/comet.png" alt="">
+      </div>
+      <div class="items">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      </div>
+      <div class="console">
+        <ul>
+          <li>Skoda, ale aspon mi ostala flasa.</li>
+          <li>Mesacny lektvar.</li>
+          <li>Mnam, banan.</li>
+          <li>Hmm nic zaujimave, musim sa rozhliadnut.</li>
+          <li>AAAAh! To sa ruti rovno na nas!</li>
+        </ul>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script setup>
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.interface {
+  padding: 1rem;
+  height: 100%;
+  display: flex;
+  justify-content: center;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.interface-main {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.viewport {
+  flex: 1;
+  height: calc(100vh - 17rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.console ul {
+  padding: 0;
+  list-style: none;
+  height: 10rem;
+}
+.items {
+  height: 4rem;
 }
 </style>

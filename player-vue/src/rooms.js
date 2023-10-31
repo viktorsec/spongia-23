e130f4cccd6e3a65e667f0c844bd880b93b47600
Laco Papay <lacop@lacop.net>
2023-10-31 17:10:54 +0100

    migrate & add a1_observatory_kitchen

--- player-vue/src/rooms.js
+++ player-vue/src/rooms.js
@@ -1,11 +1,13 @@
-import a1_spawn from '../../content/a1_spawn/a1_spawn.json';
-import a1_observatory_middle from '../../content/a1_observatory_middle/a1_observatory_middle.json';
+import a1_observatory_kitchen from '../../content/a1_observatory_kitchen/a1_observatory_kitchen.json';
 import a1_observatory_left from '../../content/a1_observatory_left/a1_observatory_left.json';
+import a1_observatory_middle from '../../content/a1_observatory_middle/a1_observatory_middle.json';
+import a1_spawn from '../../content/a1_spawn/a1_spawn.json';
 
 const rooms = [
-  a1_spawn,
-  a1_observatory_middle,
+  a1_observatory_kitchen,
   a1_observatory_left,
+  a1_observatory_middle,
+  a1_spawn,
 ];
 
 export default rooms;

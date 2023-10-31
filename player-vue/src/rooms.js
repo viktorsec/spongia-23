60c6d10e80ee89faa86df355f4b2ae1fae87bdcb
Laco Papay <lacop@lacop.net>
2023-10-31 17:28:02 +0100

    migrate all telescope rooms

--- player-vue/src/rooms.js
+++ player-vue/src/rooms.js
@@ -2,12 +2,20 @@ import a1_observatory_kitchen from '../../content/a1_observatory_kitchen/a1_obse
 import a1_observatory_left from '../../content/a1_observatory_left/a1_observatory_left.json';
 import a1_observatory_middle from '../../content/a1_observatory_middle/a1_observatory_middle.json';
 import a1_spawn from '../../content/a1_spawn/a1_spawn.json';
+import a1_telescope_comet from '../../content/a1_telescope_comet/a1_telescope_comet.json';
+import a1_telescope_empty from '../../content/a1_telescope_empty/a1_telescope_empty.json';
+import a1_telescope_moon from '../../content/a1_telescope_moon/a1_telescope_moon.json';
+import a1_telescope_planet from '../../content/a1_telescope_planet/a1_telescope_planet.json';
 
 const rooms = [
   a1_observatory_kitchen,
   a1_observatory_left,
   a1_observatory_middle,
   a1_spawn,
+  a1_telescope_comet,
+  a1_telescope_empty,
+  a1_telescope_moon,
+  a1_telescope_planet,
 ];
 
 export default rooms;

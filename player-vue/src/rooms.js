789c91bdc2249f4d6f398506576afc3d2b24e74a
Laco Papay <lacop@lacop.net>
2023-11-02 18:47:14 +0100

    idols draft

--- player-vue/src/rooms.js
+++ player-vue/src/rooms.js
@@ -8,10 +8,13 @@ import a1_telescope_empty from '../../content/a1_telescope_empty/a1_telescope_em
 import a1_telescope_moon from '../../content/a1_telescope_moon/a1_telescope_moon.json';
 import a1_telescope_planet from '../../content/a1_telescope_planet/a1_telescope_planet.json';
 import a2_forest_path from '../../content/a2_forest_path/a2_forest_path.json';
+import a2_idols from '../../content/a2_idols/a2_idols.json';
+import a2_idols_guardian from '../../content/a2_idols_guardian/a2_idols_guardian.json';
 import a2_observatory_exterior from '../../content/a2_observatory_exterior/a2_observatory_exterior.json';
 import a2_town_left from '../../content/a2_town_left/a2_town_left.json';
 import a2_town_right from '../../content/a2_town_right/a2_town_right.json';
 import a2_weird_guy from '../../content/a2_weird_guy/a2_weird_guy.json';
+import a2_weird_guy_art from '../../content/a2_weird_guy_art/a2_weird_guy_art.json';
 
 const rooms = [
   a1_observatory_kitchen,
@@ -24,9 +27,12 @@ const rooms = [
   a1_telescope_moon,
   a1_telescope_planet,
   a2_forest_path,
+  a2_idols_guardian,
+  a2_idols,
   a2_observatory_exterior,
   a2_town_left,
   a2_town_right,
+  a2_weird_guy_art,
   a2_weird_guy,
 ];
 

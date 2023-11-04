c8cc377fcd6bad7f3724bf96a9b3124ebb242e22
lacop <lacop@lacop.net>
2023-11-04 10:31:58 +0000

    cannon logic

--- player-vue/src/store/gameState.js
+++ player-vue/src/store/gameState.js
@@ -2,9 +2,9 @@ import { reactive } from 'vue';
 
 const gameState = reactive({
   // currentRoom: 'a1_spawn',
-  currentRoom: 'a2_town_right',
+  currentRoom: 'a3_cannon',
   visitedRooms: [],
-  items: [],
+  items: ['barrel'],
   itemsTaken: [],
   flags: ['dud_level_3'], // TODO: move this to content
   console: [],

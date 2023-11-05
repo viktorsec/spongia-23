e34aba3d4ba0684d92eba3f12122703862cb147a
mravcak <alex.mravcak@gmail.com>
2023-11-05 13:54:34 +0100

    move a1_spawn to a0_inside

--- player-vue/src/rooms.js
+++ player-vue/src/rooms.js
@@ -7,7 +7,6 @@ import a1_observatory_kitchen from '../../content/a1_observatory_kitchen/a1_obse
 import a1_observatory_left from '../../content/a1_observatory_left/a1_observatory_left.json';
 import a1_observatory_middle from '../../content/a1_observatory_middle/a1_observatory_middle.json';
 import a1_observatory_right from '../../content/a1_observatory_right/a1_observatory_right.json';
-import a1_spawn from '../../content/a1_spawn/a1_spawn.json';
 import a1_telescope_comet from '../../content/a1_telescope_comet/a1_telescope_comet.json';
 import a1_telescope_empty from '../../content/a1_telescope_empty/a1_telescope_empty.json';
 import a1_telescope_moon from '../../content/a1_telescope_moon/a1_telescope_moon.json';
@@ -48,7 +47,6 @@ const rooms = [
   a1_observatory_left,
   a1_observatory_middle,
   a1_observatory_right,
-  a1_spawn,
   a1_telescope_comet,
   a1_telescope_empty,
   a1_telescope_moon,

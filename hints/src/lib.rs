42e224dfadb8181899fab9a6a72991389d8dbb96
Laco Papay <lacop@lacop.net>
2023-11-15 20:52:01 +0100

    skeleton for wasm hints

---
+++ hints/src/lib.rs
@@ -0,0 +1,370 @@
+use std::collections::{BTreeMap, HashMap, VecDeque};
+
+#[derive(Debug)]
+pub struct HintGenerator {
+    rooms: BTreeMap<String, compact::Room>,
+    flag_masks: BTreeMap<String, u64>,
+    item_masks: BTreeMap<String, u64>,
+    // TODO flag/id mappings
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct State<'a> {
+    room: &'a str,
+    flags: u64,
+    items: u64,
+    items_taken: u64,
+    // todo action count
+}
+
+#[derive(Debug, Clone)]
+pub enum Transition {
+    Zone {
+        color: String,
+        trigger_item: Option<u64>,
+    },
+    // TODO taking items
+}
+
+fn create_mask<T>(map: &mut BTreeMap<String, u64>, key: T) -> u64
+where
+    T: AsRef<str>,
+{
+    let len = map.len();
+    assert!(len <= 63, "too many flags/items");
+    *map.entry(key.as_ref().to_string()).or_insert(1 << len)
+}
+
+fn create_mask_set<T, U>(map: &mut BTreeMap<String, u64>, keys: T) -> u64
+where
+    T: IntoIterator<Item = U>,
+    U: AsRef<str>,
+{
+    let mut mask = 0;
+    for key in keys {
+        mask |= create_mask(map, key.as_ref());
+    }
+    mask
+}
+
+fn get_mask<T>(map: &BTreeMap<String, u64>, key: T) -> u64
+where
+    T: AsRef<str>,
+{
+    *map.get(&key.as_ref().to_string()).unwrap()
+}
+
+fn get_mask_set<T, U>(map: &BTreeMap<String, u64>, keys: T) -> u64
+where
+    T: IntoIterator<Item = U>,
+    U: AsRef<str>,
+{
+    let mut mask = 0;
+    for key in keys {
+        mask |= get_mask(map, key.as_ref());
+    }
+    mask
+}
+
+fn apply_action<'a>(state: &mut State<'a>, action: &'a compact::Action) {
+    if let Some(goto) = &action.goto {
+        state.room = &goto;
+    }
+
+    state.flags |= action.set_flags;
+    state.flags &= !action.unset_flags;
+    state.flags ^= action.flip_flags;
+
+    state.items |= action.give_items;
+    state.items &= !action.take_items;
+}
+
+impl HintGenerator {
+    pub fn new(serialized_rooms: &str) -> HintGenerator {
+        // Parse the raw format.
+        let raw_rooms: Vec<raw::Room> = serde_json::from_str(serialized_rooms).unwrap();
+
+        // Convert to a more compact representation.
+        let mut rooms = BTreeMap::new();
+        let mut flag_masks = BTreeMap::new();
+        let mut item_masks = BTreeMap::new();
+
+        for raw_room in raw_rooms {
+            let mut room = compact::Room {
+                items: create_mask_set(
+                    &mut item_masks,
+                    raw_room.items.iter().map(|item| item.id.as_str()),
+                ),
+                // TODO enter/leave/count
+                zones: BTreeMap::new(),
+            };
+            for raw_zone in raw_room.zones {
+                let mut actions = Vec::new();
+                for raw_action in raw_zone.actions {
+                    actions.push(compact::Action {
+                        conditions: compact::Conditions {
+                            has_flags: create_mask_set(
+                                &mut flag_masks,
+                                raw_action.conditions.has_flags,
+                            ),
+                            has_flags_not: create_mask_set(
+                                &mut flag_masks,
+                                raw_action.conditions.has_flags_not,
+                            ),
+                            has_items: raw_action
+                                .conditions
+                                .has_item
+                                .map(|item| create_mask(&mut item_masks, item))
+                                .unwrap_or(0),
+                            has_items_not: raw_action
+                                .conditions
+                                .has_item_not
+                                .map(|item| create_mask(&mut item_masks, item))
+                                .unwrap_or(0),
+                        },
+                        trigger: raw_action
+                            .trigger
+                            .map(|item| create_mask(&mut item_masks, item))
+                            .unwrap_or(0),
+
+                        goto: raw_action.goto,
+                        give_items: raw_action
+                            .give_item
+                            .map(|item| create_mask(&mut item_masks, &item))
+                            .unwrap_or(0),
+                        take_items: raw_action
+                            .take_item
+                            .map(|item| create_mask(&mut item_masks, &item))
+                            .unwrap_or(0),
+                        set_flags: create_mask_set(&mut flag_masks, raw_action.set_flags),
+                        unset_flags: create_mask_set(&mut flag_masks, raw_action.unset_flags),
+                        flip_flags: create_mask_set(&mut flag_masks, raw_action.flip_flags),
+                    });
+                }
+                room.zones.insert(raw_zone.color, actions);
+            }
+
+            rooms.insert(raw_room.id, room);
+        }
+
+        //dbg!(
+        HintGenerator {
+            rooms,
+            flag_masks,
+            item_masks,
+        }
+        //)
+    }
+
+    // TODO return type. we want state->transition, also both must be something
+    // js can parse, so need a json stringify wrapper
+    pub fn find_path<'a>(
+        &'a self,
+        start_room: &'a str,
+        start_flags: &[&str],
+        start_items: &[&str],
+        start_items_taken: &[&str],
+        end_room: &str,
+    ) -> Option<Vec<Transition>> {
+        let start_state = State {
+            room: start_room,
+            flags: get_mask_set(&self.flag_masks, start_flags),
+            items: get_mask_set(&self.item_masks, start_items),
+            items_taken: get_mask_set(&self.item_masks, start_items_taken),
+        };
+
+        let mut queue = VecDeque::new();
+
+        use fasthash::city::Hash64;
+        use fasthash::RandomState;
+        let random_state = RandomState::<Hash64>::new();
+        let mut previous: HashMap<_, Option<(Transition, State)>, _> =
+            HashMap::with_hasher(random_state);
+
+        queue.push_back(start_state.clone());
+        previous.insert(start_state.clone(), None);
+
+        while !queue.is_empty() {
+            let state = queue.pop_front().unwrap();
+
+            if state.room == end_room {
+                println!("FOUND after {} states", previous.len());
+                let mut path = Vec::new();
+                let mut state = state;
+                while let Some((ref transition, ref previous_state)) = previous[&state] {
+                    path.push(transition.clone());
+                    state = previous_state.clone();
+                }
+                return Some(path);
+            }
+
+            let current_room = &self.rooms[state.room];
+            let mut maybe_add_state = |new_state: State<'a>, transition: Transition| {
+                if new_state == state {
+                    return;
+                }
+                match previous.entry(new_state) {
+                    std::collections::hash_map::Entry::Occupied(_) => {}
+                    std::collections::hash_map::Entry::Vacant(entry) => {
+                        queue.push_back(entry.key().clone());
+                        entry.insert(Some((transition, state.clone())));
+                    }
+                }
+            };
+
+            // TODO take items
+
+            for (color, actions) in current_room.zones.iter() {
+                for action in actions {
+                    // TODO hack backtracking
+
+                    // Item trigger.
+                    let mut trigger_item = None;
+                    if action.trigger != 0 {
+                        if state.items & action.trigger == 0 {
+                            continue;
+                        }
+                        trigger_item = Some(action.trigger);
+                    }
+                    // Conditions.
+                    if state.flags & action.conditions.has_flags != action.conditions.has_flags {
+                        continue;
+                    }
+                    if state.flags & action.conditions.has_flags_not != 0 {
+                        continue;
+                    }
+                    if state.items & action.conditions.has_items != action.conditions.has_items {
+                        continue;
+                    }
+                    if state.items & action.conditions.has_items_not != 0 {
+                        continue;
+                    }
+
+                    // Eligible action at this point, apply it.
+                    let mut news_state = state.clone();
+                    apply_action(&mut news_state, action);
+                    // TODO leave/enter, action counter
+
+                    maybe_add_state(
+                        news_state,
+                        Transition::Zone {
+                            color: color.clone(),
+                            trigger_item,
+                        },
+                    );
+                }
+            }
+        }
+
+        None
+    }
+}
+
+mod compact {
+    use std::collections::BTreeMap;
+
+    #[derive(Debug)]
+    pub struct Room {
+        pub items: u64,
+        pub zones: BTreeMap<String, Vec<Action>>,
+        // TODO enter/leave actions and max count
+    }
+
+    #[derive(Debug)]
+    pub struct Action {
+        pub conditions: Conditions,
+        pub trigger: u64,
+
+        pub goto: Option<String>,
+        pub give_items: u64,
+        pub take_items: u64,
+
+        pub set_flags: u64,
+        pub unset_flags: u64,
+        pub flip_flags: u64,
+    }
+
+    #[derive(Debug)]
+    pub struct Conditions {
+        pub has_flags: u64,
+        pub has_flags_not: u64,
+        pub has_items: u64,
+        pub has_items_not: u64,
+    }
+}
+
+mod raw {
+    use serde::Deserialize;
+    use std::collections::BTreeSet;
+
+    #[derive(Deserialize, Debug)]
+    pub struct Room {
+        pub id: String,
+        #[serde(default)]
+        pub items: Vec<Item>,
+        #[serde(default)]
+        pub zones: Vec<Zone>,
+
+        #[serde(default, rename = "enterAction")]
+        enter_action: Option<Action>,
+        #[serde(default, rename = "leaveAction")]
+        leave_action: Option<Action>,
+        #[serde(default, rename = "maxActions")]
+        max_actions: Option<usize>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct Item {
+        pub id: String,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct Zone {
+        pub color: String,
+        pub actions: Vec<Action>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct Action {
+        #[serde(default)]
+        pub trigger: Option<String>,
+
+        #[serde(default)]
+        pub conditions: Conditions,
+
+        #[serde(default)]
+        pub goto: Option<String>,
+        #[serde(default, rename = "giveItem")]
+        pub give_item: Option<String>,
+        #[serde(default, rename = "takeItem")]
+        pub take_item: Option<String>,
+
+        #[serde(default, rename = "setFlag", deserialize_with = "comma_separated")]
+        pub set_flags: BTreeSet<String>,
+        #[serde(default, rename = "unsetFlag", deserialize_with = "comma_separated")]
+        pub unset_flags: BTreeSet<String>,
+        #[serde(default, rename = "flipFlag", deserialize_with = "comma_separated")]
+        pub flip_flags: BTreeSet<String>,
+    }
+
+    #[derive(Deserialize, Debug, Default)]
+    pub struct Conditions {
+        #[serde(default, rename = "hasFlags", deserialize_with = "comma_separated")]
+        pub has_flags: BTreeSet<String>,
+        #[serde(default, rename = "hasFlagsNot", deserialize_with = "comma_separated")]
+        pub has_flags_not: BTreeSet<String>,
+
+        #[serde(rename = "hasItem")]
+        pub has_item: Option<String>,
+        #[serde(rename = "hasItemNot")]
+        pub has_item_not: Option<String>,
+    }
+
+    fn comma_separated<'de, D>(deserializer: D) -> Result<BTreeSet<String>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(s.split(',').map(|s| s.to_owned()).collect())
+    }
+}
---
+++ hints/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let content_dir = std::env::args()
+        .nth(1)
+        .expect("Requires one argument for the content directory");
+
+    let start_room = std::env::args().nth(2).unwrap_or("a0_inside".to_string());
+    let end_room = std::env::args()
+        .nth(3)
+        .unwrap_or("a2_forest_path".to_string()); // TODO
+
+    // TODO start flags/items
+
+    // Concat to JSON array. That is what wasm will get.
+    let mut rooms = Vec::new();
+    for subdir in std::fs::read_dir(content_dir).unwrap() {
+        let subdir = subdir.unwrap();
+        if !subdir.file_type().unwrap().is_dir() {
+            continue;
+        }
+        for entry in std::fs::read_dir(subdir.path()).unwrap() {
+            let entry = entry.unwrap().path();
+            if entry.extension().unwrap_or_default() != "json" {
+                continue;
+            }
+            if entry
+                .file_stem()
+                .map(std::ffi::OsStr::to_str)
+                .flatten()
+                .map(|stem| stem.ends_with(".original"))
+                .unwrap_or(false)
+            {
+                continue;
+            }
+
+            rooms.push(std::fs::read_to_string(entry).unwrap());
+        }
+    }
+
+    let generator = hints::HintGenerator::new(&format!("[{}]", &rooms.join(",")));
+
+    let path = generator.find_path(
+        &start_room,
+        &vec!["dud_level_3"],
+        &vec![],
+        &vec![],
+        &end_room,
+    );
+    if let Some(path) = path {
+        println!("Path: {:#?}", path);
+    } else {
+        println!("No path found");
+    }
+}
